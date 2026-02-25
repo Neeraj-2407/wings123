3e8f485720141b8dcd100014b384bcc87da54a96
Neeraj-2407 <neerajwings1@gmail.com>
2026-02-25 17:13:24 +0530

    boolean values

---
+++ python_booleans/booleans.py
@@ -0,0 +1,20 @@
+print(18 > 22)
+print(11 == 9)
+print(10 < 91)
+
+a = 18
+b = 45
+
+if b > a:
+  print("b is greater than a")
+else:
+  print("b is not greater than a")
+
+print(bool("Hello"))
+print(bool())
+
+a = "Neeraj"
+b = ()
+
+print(bool(a))
+print(bool(b))
