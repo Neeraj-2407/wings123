30e6fc965ef275218847ce5bd94074d15499b3d2
Neeraj-2407 <neerajwings1@gmail.com>
2026-02-25 17:47:23 +0530

    op variables

---
+++ python_variables/output_variables/op_variables.py
@@ -0,0 +1,22 @@
+x = "Python is high-level programming language"
+print(x)
+
+a = "Python "
+b = "is "
+c = "high-level "
+d = "programming language"
+print(a , b, c, d)
+
+a = "Python "
+b = "is "
+c = "high-level "
+d = "programming language"
+print(a+b+c+d)
+
+a = 10
+b = 20
+print(a + b)
+
+a = 10
+b = "Neeraj"
+print(a , b)
