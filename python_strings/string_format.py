94d0202add73c4fade2bc65a9e13e361df1da18e
Neeraj-2407 <neerajwings1@gmail.com>
2026-02-25 17:45:01 +0530

    string format

---
+++ python_strings/string_format.py
@@ -0,0 +1,14 @@
+age = 22
+txt = f"My name is Neeraj, I am {age}" #we cannot combine the letters and numbers
+print(txt)
+
+price = 70
+txt = f"The price is {price} rupees" #place holders and modifiers
+print(txt)
+
+price = 909
+txt = f"The price is {price:.2f} rupees"
+print(txt)
+
+txt = f"The price is {20 * 70} cad"
+print(txt)
