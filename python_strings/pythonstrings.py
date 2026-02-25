f46b6f344f9b344b723c95e0ea7f736f239c5bbe
Neeraj-2407 <neerajwings1@gmail.com>
2026-02-25 17:43:46 +0530

    python strings

---
+++ python_strings/pythonstrings.py
@@ -0,0 +1,31 @@
+a = "Neeraj" #Assign String to a Variable
+print(a)
+
+#multiple strings
+a = """This is  
+a multi-line
+string"""
+print(a)
+
+a = "Neeraj"
+print(a[3])
+
+for x in "Neeraj": #Looping Through a String
+  print(x) 
+
+a = "Neeraj" #stringlength
+print(len(a))
+
+txt = "python is high-level language" #check if string is present
+print("is" in txt)
+
+txt = "python is high-level language" #check if string is not present
+print("java" not in txt)
+
+txt = "python is high-level language" #check if string is present
+print("is" in txt)
+print("Yes, 'is' is present.")
+
+txt = "python is high-level language" #check if string is not present
+print("java" not in txt)
+print("No, 'java' is NOT present.")
