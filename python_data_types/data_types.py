bfa656b491717cd9e5b895d6693c93e68363962f
Neeraj-2407 <neerajwings1@gmail.com>
2026-02-25 17:18:58 +0530

    data types

---
+++ python_data_types/data_types.py
@@ -0,0 +1,45 @@
+a = "Neeraj" #str
+print(a)
+print(type(a)) 
+a = 18       #int   
+print(a)
+print(type(a))  
+a = 18.0     #float
+print(a)
+print(type(a))
+a = 1j      #complex
+print(a)
+print(type(a))
+a = ["pizza", "burger", "taco"]  #list
+print(a)
+print(type(a))
+a = ("summer", "winter", "spring") #tuple
+print(a)
+print(type(a))
+a = range(8) #range
+print(a)
+print(list(a))
+a = {"city" : "London", "country" : "UK"} #dict
+print(a)
+print(type(a))
+a = {"red", "green", "blue"} #set
+print(a)
+print(type(a))
+a = frozenset({"gold", "silver", "bronze"}) #frozenset
+print(a)
+print(type(a))
+a = True  #bool
+print(a)
+print(type(a))
+a = b"Python" #byte
+print(a)
+print(type(a))
+a = bytearray(7) #bytearray
+print(a)
+print(type(a))
+a = memoryview(bytes(3)) #memoryview
+print(a)
+print(type(a))
+a = None #nonetype
+print(a)
+print(type(a))
