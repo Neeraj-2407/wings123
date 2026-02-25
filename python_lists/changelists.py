b3db22cbc45f75233823440fcf130a2bb6f92d5d
Neeraj6618 <neerajsainandigama04@gmail.com>
2026-02-25 18:52:00 +0530

    change lists

---
+++ python_lists/changelists.py
@@ -0,0 +1,15 @@
+x = ["neeraj","sai","rohit"]
+x[1]="vamshi"
+print(x)
+
+x = ["neeraj","sai","rohit","prem","raghav","vamshi"]
+x[1:3]=["nipun","satya"]
+print(x)
+
+x = ["neeraj","sai","rohit","prem","raghav","vamshi"]
+x.insert(2,"rupa")
+print(x)
+
+x = ["neeraj","sai","rohit","prem","raghav","vamshi"]
+x[1:4]=["sanjay"]
+print(x)
