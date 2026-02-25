e6bef53d8aa563f7f32441facac2dd3317ebe06a
Neeraj-2407 <neerajwings1@gmail.com>
2026-02-25 17:17:33 +0530

    casting

---
+++ python_casting/casting.py
@@ -0,0 +1,21 @@
+x = int(10) #converting all into integers
+y = int(2.6)
+z = int("8")
+print(x)
+print(y)
+print(z)
+
+x = float(3)#converting all into decimal
+y = float(7.8)
+z = float("7")
+print(x)
+print(y)
+print(z)
+
+x = str("Neeraj")
+y = str(22)
+z = str(3.9)
+print(x)
+print(y)
+print(z)
+
