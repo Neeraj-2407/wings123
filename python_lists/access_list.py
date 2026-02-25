8ea1d196749220cd3db4e596508c7eb78e423bc0
Neeraj6618 <neerajsainandigama04@gmail.com>
2026-02-25 18:21:31 +0530

    access list

---
+++ python_lists/access_list.py
@@ -0,0 +1,14 @@
+x = ["neeraj","sai","rohit"]
+print(x[1])
+
+x = ["neeraj","sai","rohit"] #negative indexing
+print(x[-1])
+
+x = ["neeraj","sai","rohit","prem","raghav","vamshi"] #range of index
+print(x[1:5])
+
+x = ["neeraj","sai","rohit","prem","raghav","vamshi"] #range of index
+print(x[:3])
+
+x = ["neeraj","sai","rohit","prem","raghav","vamshi"] #range of index
+print(x[3:])
