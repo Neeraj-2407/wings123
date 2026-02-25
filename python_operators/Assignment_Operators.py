1d0ded9f2e5257113eb597eb16a0245e46209308
Neeraj-2407 <neerajwings1@gmail.com>
2026-02-25 17:35:18 +0530

    assignment

---
+++ python_operators/Assignment_Operators.py
@@ -0,0 +1,37 @@
+x = 5	
+x += 3
+x = 5
+x -= 3	
+x = 5	
+x *= 3	
+x = 5
+x /= 3	
+x = 5 	
+x %= 3
+x = 5	
+x //= 3	
+x = 5	
+x **= 3	
+x = 5
+x &= 3	
+x = 5
+x |= 3	
+x = 5	
+x ^= 3	
+x = 5 
+x >>= 3	
+x = 5 
+x <<= 3	
+print(x)
+print(x)
+print(x)
+print(x)
+print(x)
+print(x)
+print(x)
+print(x)
+print(x)
+print(x)
+print(x)
+print(x)
+print(x)
