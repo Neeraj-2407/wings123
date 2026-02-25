0aa7262d7c8091fd3ebfe3dea2b634dd5a43b23c
Neeraj6618 <neerajsainandigama04@gmail.com>
2026-02-25 18:28:44 +0530

    added negative range of index

--- python_lists/access_list.py
+++ python_lists/access_list.py
@@ -7,8 +7,11 @@ print(x[-1])
 x = ["neeraj","sai","rohit","prem","raghav","vamshi"] #range of index
 print(x[1:5])
 
-x = ["neeraj","sai","rohit","prem","raghav","vamshi"] #range of index
+x = ["neeraj","sai","rohit","prem","raghav","vamshi"] #range of index beginning
 print(x[:3])
 
-x = ["neeraj","sai","rohit","prem","raghav","vamshi"] #range of index
+x = ["neeraj","sai","rohit","prem","raghav","vamshi"] #range of index end
 print(x[3:])
+
+x = ["neeraj","sai","rohit","prem","raghav","vamshi"] #range of index(negative)
+print(x[-3:-1])
