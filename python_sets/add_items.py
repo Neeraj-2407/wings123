59453eaaa3771d2f3bb0b1bdbb572f9c9aed9d1f
Neeraj-2407 <neerajwings1@gmail.com>
2026-02-26 15:04:12 +0530

    adding items to set

---
+++ python_sets/add_items.py
@@ -0,0 +1,14 @@
+# a = {"pen","pencil","scale"}
+# a.add("eraser")
+# print(a)
+# #adding another set to current set
+# a = {"pen","pencil","scale"}
+# b = {"orange","banana","apple"}
+# a.update(b) 
+# print(a)
+# #adding list to current set
+
+a={"pen","pencil","scale"}
+b=["orange","apple","banana"]
+a.update(b)
+print(a)
