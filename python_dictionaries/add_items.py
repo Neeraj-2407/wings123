eb1e456b60bb80f213edf33dcb5bf7cf6e53cd2a
Neeraj-2407 <neerajwings1@gmail.com>
2026-02-26 18:37:00 +0530

    adding items

--- python_dictionaries/add_items.py
+++ python_dictionaries/add_items.py
@@ -1,7 +1,17 @@
+#adding an extra key value pair
 x = {
    "name" : "neeraj",
    "roll no" : 6618,
    "branch" : "cse"
 }
 x["year"]= 4
+print(x)
+
+#updating a key using update()
+x = {
+   "name" : "neeraj",
+   "roll no" : 6618,
+   "branch" : "cse"
+}
+x.update({"clg": "aurora"})
 print(x)
