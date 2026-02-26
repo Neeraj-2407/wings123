5c0f25746cd9fe69af562a35fa0bb16dfee65d7f
Neeraj6618 <neerajsainandigama04@gmail.com>
2026-02-26 22:38:16 +0530

    nested dictionaries

---
+++ python_dictionaries/nested_dictionaries.py
@@ -0,0 +1,74 @@
+# csea = {
+#   student1 : {
+#     "name" : "neeraj",
+#     "year" : 4
+#   },
+#   student2 : {
+#     "name" : "prem",
+#     "year" : 4
+#   },
+#   student3 : {
+#     "name" : "rohit",
+#     "year" : 3
+#   }
+# }
+# print(csea)
+
+
+# student1 = {
+#     "name" : "neeraj",
+#     "year" : 4
+#   }
+# student2 = {
+#     "name" : "prem",
+#     "year" : 4
+#   }
+# student3 = {
+#     "name" : "rohit",
+#     "year" : 3
+# }   
+
+# csea = {
+#     "student1":student1,
+#     "student2":student2,
+#     "student3":student3
+# }
+# print(csea)
+
+#accessing the items
+# csea = {
+#   "student1" : {
+#     "name" : "neeraj",
+#     "year" : 4
+#   },
+#   "student2" : {
+#     "name" : "prem",
+#     "year" : 4
+#   },
+#   "student3" : {
+#     "name" : "rohit",
+#     "year" : 3
+#   }
+# }
+# print(csea["student3"]["name"])
+
+
+csea = {
+  "student1" : {
+    "name" : "neeraj",
+    "year" : 4
+  },
+  "student2" : {
+    "name" : "prem",
+    "year" : 4
+  },
+  "student3" : {
+    "name" : "rohit",
+    "year" : 3
+  }
+} 
+
+for x , object in csea.items():
+    print(x)
+    for y in object:
+        print(y +':',object[y])
