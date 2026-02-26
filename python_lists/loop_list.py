f22e81e730e863cf982388ebae534b958c5c2231
Neeraj6618 <neerajsainandigama04@gmail.com>
2026-02-26 06:35:28 +0530

    looping numbers

--- python_lists/loop_list.py
+++ python_lists/loop_list.py
@@ -1,3 +1,16 @@
-b = ["Neeraj","sai","vamshi"]
+b = ["Neeraj","sai","vamshi"] #loop through list
 for a in b:
     print(a)
+
+x = ["Neeraj","sai","vamshi"] #looping through the index numbers
+for i in range(len(x)):
+  print(x[i])
+
+x = ["Neeraj","sai","vamshi"]
+i = 1
+while i < len(x):
+  print(x[i])
+  i = i + 1
+
+x = ["Neeraj","sai","vamshi"] #it prints all the values in the list
+[print(y) for y in x]
