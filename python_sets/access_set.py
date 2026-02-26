a9354027c62a0e781b17dc3fac8fff9c0723cad1
Neeraj-2407 <neerajwings1@gmail.com>
2026-02-26 14:39:20 +0530

    accessing the elements

---
+++ python_sets/access_set.py
@@ -0,0 +1,9 @@
+a = {"pen","pencil","cherry"}
+for x in a:
+    print(x)
+
+a = {"pen","pencil","cherry"} #in (present)
+print("pen" in a)
+
+a = {"pen","pencil","cherry"} #Not in (not present)
+print("pen" not in a)
