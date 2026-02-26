d93e777c575e5a3da10bfe09a178cfed67d8ebfe
Neeraj6618 <neerajsainandigama04@gmail.com>
2026-02-26 21:12:14 +0530

    removed some commits

--- python_dictionaries/access_dictionaries.py
+++ python_dictionaries/access_dictionaries.py
@@ -1,69 +1,69 @@
 #it stores value in a and then it will print
-# x = {
-#     "name"    : "neeraj",
-#     "roll no" :  6618,
-#     "branch"  : "cse"
-# }
-# a = x["branch"]
-# print(a)
+x = {
+    "name"    : "neeraj",
+    "roll no" :  6618,
+    "branch"  : "cse"
+}
+a = x["branch"]
+print(a)
 
 #we can print the value by using get()
-# x = {
-#     "name"    : "neeraj",
-#     "roll no" :  6618,
-#     "branch"  : "cse"
-# }
-# a = x.get("name")
-# print(a)
+x = {
+    "name"    : "neeraj",
+    "roll no" :  6618,
+    "branch"  : "cse"
+}
+a = x.get("name")
+print(a)
 
 #by using keys() we can print values
-# x = {
-#     "name"    : "neeraj",
-#     "roll no" :  6618,
-#     "branch"  : "cse"
-# }
-# a = x.keys()
-# print(a)
+x = {
+    "name"    : "neeraj",
+    "roll no" :  6618,
+    "branch"  : "cse"
+}
+a = x.keys()
+print(a)
 
 #by using values() we can print keys
-# x = {
-#     "name"    : "neeraj",
-#     "roll no" :  6618,
-#     "branch"  : "cse"
-# }
-# a = x.values()
-# print(a)
+x = {
+    "name"    : "neeraj",
+    "roll no" :  6618,
+    "branch"  : "cse"
+}
+a = x.values()
+print(a)
 
 #adding an extra key value pair and printing keys
-# detail = {
-#     "name"    : "neeraj",
-#     "roll no" :  6618,
-#     "branch"  : "cse"
-# }
-# a = detail.keys()
-# print(a) #before the change
-# detail["year"]= 4
-# print(a) #after the change
+detail = {
+    "name"    : "neeraj",
+    "roll no" :  6618,
+    "branch"  : "cse"
+}
+a = detail.keys()
+print(a) #before the change
+detail["year"]= 4
+print(a) #after the change
 
 #adding an extra key value pair and printing values
-# detail = {
-#     "name"    : "neeraj",
-#     "roll no" :  6618,
-#     "branch"  : "cse"
-# }
-# a = detail.values()
-# print(a) #before the change
-# detail["year"]= 4
-# print(a) #after the change
+detail = {
+    "name"    : "neeraj",
+    "roll no" :  6618,
+    "branch"  : "cse"
+}
+a = detail.values()
+print(a) #before the change
+detail["year"]= 4
+print(a) #after the change
 
 #printing total items by using items()
-# x = {
-#     "name"    : "neeraj",
-#     "roll no" :  6618,
-#     "branch"  : "cse"
-# }
-# a = x.items()
-# print(a)
+x = {
+    "name"    : "neeraj",
+    "roll no" :  6618,
+    "branch"  : "cse"
+}
+a = x.items()
+print(a)
 
 #checking if key exist or not by using if statement
 x = {
