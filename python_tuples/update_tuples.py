1c1facde94b0abfc371a640c11f0608b0cd95e9f
Neeraj-2407 <neerajwings1@gmail.com>
2026-02-26 11:19:44 +0530

    .\update_tuples.py

--- python_tuples/update_tuples.py
+++ python_tuples/update_tuples.py
@@ -4,8 +4,13 @@ b[2] = "prem"
 a = tuple(b)
 print(a)
 
-a = ("Neeraj","sai","vamshi","teja")
+a = ("Neeraj","sai","vamshi","teja") #add an item using append
 b = list(a)
 b.append("nipun")
 a = tuple(b)
 print(a)
+
+x = ("neeraj","sai","prem") #adding tuple to tuple
+y = ("saikiran",)
+x += y
+print(x)
