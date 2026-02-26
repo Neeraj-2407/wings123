15849027a210f57bbeb6c692191cda50d914bd95
Neeraj-2407 <neerajwings1@gmail.com>
2026-02-26 14:27:55 +0530

    set basics

---
+++ python_sets/set.py
@@ -0,0 +1,17 @@
+a = {"pen","pencil","cherry"}
+print(a)
+
+a = {"pen","pencil","cherry","pen","scale","paper"} #no duplicates allowed
+print (a)
+
+a = {"pen","pencil","cherry",1,True,2,"pen","scale","paper"} #True==1
+print (a)
+
+a = {"pen","pencil","cherry",1,False,2,0,"pen","scale","paper"} #False==0
+print (a)
+
+a = {"pen","pencil","cherry","pen","scale","paper"}
+print(len(a))
+
+a = {"pen","pencil","cherry","pen","scale","paper"}
+print(type(a))
