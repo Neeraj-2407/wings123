5fb23a024cd1b5504e406a61c32a03261ddbfdbc
Neeraj-2407 <neerajwings1@gmail.com>
2026-02-26 14:41:29 +0530

    added a new program at the end

--- python_lists/loop_comprehension.py
+++ python_lists/loop_comprehension.py
@@ -1,20 +1,27 @@
-names = ["neeraj","sai","rohit","prem","raghav","vamshi"]
-newlist = []
+# names = ["neeraj","sai","rohit","prem","raghav","vamshi"]
+# newlist = []
 
+# for x in names:
+#   if "e" in x:
+#     newlist.append(x)
+
+# print(newlist)
+
+# names = ["neeraj","sai","rohit","prem","raghav","vamshi"]
+
+# newlist = [x for x in names if "e" in x] #loops through each element
+
+# print(newlist)
+
+# names = ["neeraj","sai","rohit","prem","raghav","vamshi"]
+
+# newlist = [x for x in names if x != "neeraj"]
+
+# print(newlist)
+
+names = ["neeraj","sai","rohit","prem","raghav","vamshi"]
+newnames = [ ]
 for x in names:
   if "e" in x:
-    newlist.append(x)
-
-print(newlist)
-
-names = ["neeraj","sai","rohit","prem","raghav","vamshi"]
-
-newlist = [x for x in names if "e" in x] #loops through each element
-
-print(newlist)
-
-names = ["neeraj","sai","rohit","prem","raghav","vamshi"]
-
-newlist = [x for x in names if x != "neeraj"]
-
-print(newlist)
+    newnames.append(x)
+print(newnames)
