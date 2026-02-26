93ec8029cf4b0abdb92af9fa3c6126ef1e19e69c
Neeraj6618 <neerajsainandigama04@gmail.com>
2026-02-26 07:54:26 +0530

    tuples

---
+++ python_tuples/python_tuple.py
@@ -0,0 +1,14 @@
+x = ("Neeraj","sai","vamshi") #x is a tuple, it also allows duplicates
+print(x)
+
+x = ("Neeraj","sai","vamshi") #length of tuple
+print(len(x))
+
+x = ("Neeraj",) #comma represent \s tuple
+print(type(x))
+
+y = ("neeraj") #it is a string
+print(type(y))
+
+x = tuple(("Neeraj","sai","vamshi")) #tuple()constructor
+print(x)
