1c19939cb00b9b61bf80674b8760d52abdc721eb
Neeraj6618 <neerajsainandigama04@gmail.com>
2026-02-26 07:17:45 +0530

    copy of list

---
+++ python_lists/copy_lists.py
@@ -0,0 +1,11 @@
+x = ["Neeraj","sai","vamshi"] #using copy()method
+mylist = x.copy()
+print(mylist)
+
+x = ["Neeraj","sai","vamshi"] #using list()method
+mylist = list(x)
+print(mylist)
+
+x = ["Neeraj","sai","vamshi"] #using slice operator :
+mylist = x[:]
+print(mylist)
