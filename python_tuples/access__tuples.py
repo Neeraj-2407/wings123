fb5953904d877effa9deea54bb5aa1674116d4e5
Neeraj6618 <neerajsainandigama04@gmail.com>
2026-02-26 08:08:56 +0530

    accessing tuples

---
+++ python_tuples/access__tuples.py
@@ -0,0 +1,11 @@
+x = ("Neeraj","sai","vamshi") 
+print(x[2])
+
+x = ("Neeraj","sai","vamshi") #negative indexing
+print(x[-2])
+
+a  = ("neeraj","sai","rohit","prem","raghav","vamshi") #range of index
+print(a[2:4])
+
+a  = ("neeraj","sai","rohit","prem","raghav","vamshi") #range of negative index
+print(a[-3:-1])
