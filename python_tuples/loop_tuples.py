0b60242cf01556e03864c0ab657e728a2706e0f9
Neeraj-2407 <neerajwings1@gmail.com>
2026-02-26 12:30:38 +0530

    different types of looping

---
+++ python_tuples/loop_tuples.py
@@ -0,0 +1,13 @@
+a = ("neeraj","sai","prem") #looping through a tuple
+for x in a :
+    print(x) 
+
+a = ("pen","pencil","book","eraser") #looping through a index number
+for i in range(len(a)):
+    print(a[i])    
+
+a = ("pen","pencil","book","eraser") #looping through tuple using while loop
+i = 0
+while i < (len(a)):
+    print(a[i])
+    i = i + 1
