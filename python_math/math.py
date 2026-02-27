88edd51098567f70cf9756c694a41d54f5eb9e41
Neeraj6618 <neerajsainandigama04@gmail.com>
2026-02-27 20:40:35 +0530

    basic match functions

---
+++ python_math/math.py
@@ -0,0 +1,33 @@
+#min() and max() are built in math funtions
+a = min(5, 10, 25)
+b = max(5, 10, 25)
+
+print(a)
+print(b)
+
+#printing the absolute positive value by abs()
+a = abs(-77.32)
+print(a)
+
+#printing the value of x to the power of y by using pow()
+a = pow(3,4)
+print(a)
+
+#importing the math() function
+import math
+a = math.sqrt(144)
+print(a)
+
+#rounding the nearest nummbers using match.ceil and math.floor
+import math
+
+x = math.ceil(12.7)
+y = math.floor(11.4)
+
+print(x) 
+print(y) 
+
+#importing the value of pi
+import math
+a = math.pi
+print(a)
