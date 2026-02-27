7b92f4b69968fb8118e20138c98174e7ba227808
Neeraj6618 <neerajsainandigama04@gmail.com>
2026-02-27 06:59:14 +0530

    added a new program

--- python_dictionaries/nested_dictionaries.py
+++ python_dictionaries/nested_dictionaries.py
@@ -1,56 +1,56 @@
-csea = {
-  student1 : {
-    "name" : "neeraj",
-    "year" : 4
-  },
-  student2 : {
-    "name" : "prem",
-    "year" : 4
-  },
-  student3 : {
-    "name" : "rohit",
-    "year" : 3
-  }
-}
-print(csea)
+# csea = {
+#   student1 : {
+#     "name" : "neeraj",
+#     "year" : 4
+#   },
+#   student2 : {
+#     "name" : "prem",
+#     "year" : 4
+#   },
+#   student3 : {
+#     "name" : "rohit",
+#     "year" : 3
+#   }
+# }
+# print(csea)
 
 
-# student1 = {
-    "name" : "neeraj",
-    "year" : 4
-  }
-student2 = {
-    "name" : "prem",
-    "year" : 4
-  }
-student3 = {
-    "name" : "rohit",
-    "year" : 3
-}   
+# # student1 = {
+#     "name" : "neeraj",
+#     "year" : 4
+#   }
+# student2 = {
+#     "name" : "prem",
+#     "year" : 4
+#   }
+# student3 = {
+#     "name" : "rohit",
+#     "year" : 3
+# }   
 
-csea = {
-    "student1":student1,
-    "student2":student2,
-    "student3":student3
-}
-print(csea)
+# csea = {
+#     "student1":student1,
+#     "student2":student2,
+#     "student3":student3
+# }
+# print(csea)
 
-#accessing the items
-csea = {
-  "student1" : {
-    "name" : "neeraj",
-    "year" : 4
-  },
-  "student2" : {
-    "name" : "prem",
-    "year" : 4
-  },
-  "student3" : {
-    "name" : "rohit",
-    "year" : 3
-  }
-}
-print(csea["student3"]["name"])
+# #accessing the items
+# csea = {
+#   "student1" : {
+#     "name" : "neeraj",
+#     "year" : 4
+#   },
+#   "student2" : {
+#     "name" : "prem",
+#     "year" : 4
+#   },
+#   "student3" : {
+#     "name" : "rohit",
+#     "year" : 3
+#   }
+# }
+# print(csea["student3"]["name"])
 
 #looping the dictionary using items()
 csea = {
