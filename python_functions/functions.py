9452d5aef4ed2b2e595ded01f13ade083d995876
Neeraj-2407 <neerajwings1@gmail.com>
2026-02-27 11:38:01 +0530

    basic functions

---
+++ python_functions/functions.py
@@ -0,0 +1,33 @@
+#defining a function using def
+def neeraj():
+    print("hello all!")
+
+#calling a function 
+def neeraj():
+    print("Hello All!")
+neeraj()    
+
+#we can call the function multiple times
+def neeraj():
+    print("Hello All!")
+neeraj()    
+neeraj()
+neeraj()
+neeraj()
+
+#we can write code once,define it and we can call it multiple times
+def celsius_to_farenheit(celcius):
+    return(celcius*9/5)+32
+print(celsius_to_farenheit(32))
+print(celsius_to_farenheit(45))
+print(celsius_to_farenheit(37))
+
+#return values by using return
+def neeraj():
+    return "Hello Good Morning"
+message = neeraj()
+print(message)
+
+#we can create a function placeholder without any code by using pass statement
+def neeraj():
+    pass
