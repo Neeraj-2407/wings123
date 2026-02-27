29ccc2624a1efa6f4d5ceb4eb7c90aef0954c0f8
Neeraj6618 <neerajsainandigama04@gmail.com>
2026-02-27 06:57:42 +0530

    simple if statements

---
+++ python_ifelse/python_if.py
@@ -0,0 +1,23 @@
+# Equals: a == b
+# Not Equals: a != b
+# Less than: a < b
+# Less than or equal to: a <= b
+# Greater than: a > b
+# Greater than or equal to: a >= b
+
+a = 20
+b = 50
+if b > a:
+  print("b is greater than a")
+
+number = 20
+if number > 5:
+  print("The number is positive")  
+
+# multiple statements of if block
+score = 75
+
+if score >= 50:
+  print("You passed the exam")
+  print("You can move to the next level")
+  print("Congratulations on your success") 
