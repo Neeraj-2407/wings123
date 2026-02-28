4eabd4bce0ce94854b2c54fe451b4be0c022abfd
Neeraj6618 <neerajsainandigama04@gmail.com>
2026-02-28 07:29:48 +0530

    ädded 2 new programs

--- python_functions/python_decorators.py
+++ python_functions/python_decorators.py
@@ -12,3 +12,56 @@ def say_hello():
     print("Hello!")
 
 say_hello()
+
+#using @changecase for two decorators
+def changecase(func):
+  def myinner():
+    return func().upper()
+  return myinner
+
+@changecase
+def myfunction():
+  return "Hello Sally"
+
+@changecase
+def otherfunction():
+  return "I am speed!"
+
+print(myfunction())
+print(otherfunction())
+
+#decorators using arguments
+def changecase(n):
+  def changecase(func):
+    def myinner():
+      if n == 1:
+        a = func().lower()
+      else:
+        a = func().upper()
+      return a
+    return myinner
+  return changecase
+
+@changecase(1)
+def myfunction():
+  return "Hello neeraj"
+
+print(myfunction())
+
+#using multiple decortors
+def changecase(func):
+  def myinner():
+    return func().lower()
+  return myinner
+
+def addgreeting(func):
+  def myinner():
+    return "Hello " + func() + " Have a good day!"
+  return myinner
+
+@changecase
+@addgreeting
+def myfunction():
+  return "neeraj"
+
+print(myfunction())
