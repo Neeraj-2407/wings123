f0e796f6c8e014c1cf5eeed454eb24071af184da
Neeraj6618 <neerajsainandigama04@gmail.com>
2026-02-28 07:43:14 +0530

    added a program

--- python_functions/python_recursion.py
+++ python_functions/python_recursion.py
@@ -1,1 +1,39 @@
-z
+#a recursive function counts from 10
+def countdown(n):
+  if n <= 0:
+    print("Done!")
+  else:
+    print(n)
+    countdown(n - 1)
+
+countdown(10)
+
+#base case and recursive case
+def factorial(n):
+  # Base case
+  if n == 0 or n == 1:
+    return 1
+  # Recursive case
+  else:
+    return n * factorial(n - 1)
+
+print(factorial(9))
+
+#fibinocci sequence
+def fibonacci(n):
+  if n <= 1:
+    return n
+  else:
+    return fibonacci(n - 1) + fibonacci(n - 2)
+
+print(fibonacci(9))
+
+#recursing with lists
+def sum_list(numbers):
+  if len(numbers) == 0:
+    return 0
+  else:
+    return numbers[0] + sum_list(numbers[1:])
+
+my_list = [1, 2, 3, 4, 5,6]
+print(sum_list(my_list))
